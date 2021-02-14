dca935ae2d531b0c6a148b6a2f5663b16c5c9181
Michael Song <mjysong@gmail.com>
2021-02-14 12:34:15 -0500

    Retrieved distances from cities to parks. Also did refactoring for Distance Matrix request.

--- __main__.py
+++ __main__.py
@@ -3,11 +3,12 @@ from googlemaps import client
 from google_maps_services import GMapsServices
 from keys import API_KEY, NEW_API_KEY
 
-g_client = client.Client(key=NEW_API_KEY)
+g_client = client.Client(key=API_KEY)
 
 g_maps_services = GMapsServices(client=g_client)
 # g_maps_services.get_nps_raw_park_data()
 # g_maps_services.fill_missing_park_data()
-#top = g_maps_services.rank_places_by_reviews()
-#print(top)
-#g_maps_services.compute_distances()
+# top = g_maps_services.rank_places_by_reviews()
+# print(top)
+# g_maps_services.compute_distances_for_cities()
+g_maps_services.map_place_id_to_park_name()
