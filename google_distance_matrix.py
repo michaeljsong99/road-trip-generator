804b99449e8e67f112ec222ef266d08bb0117db8
michaeljsong99 <44045529+michaeljsong99@users.noreply.github.com>
2021-02-14 14:09:13 -0500

    Compute city distances (#2)
    * Retrieved distances from cities to parks. Also did refactoring for Distance Matrix request.
    
    * Computed nearest city for each park.

--- google_distance_matrix.py
+++ google_distance_matrix.py
@@ -19,30 +19,125 @@ class GDistanceMatrix:
     # Returns the list of origins/destinations as a string.
     def _create_places_string(self, place_list):
         query_list = [f"place_id:{place_id}" for place_id in place_list]
-        return '|'.join(query_list)
+        return "|".join(query_list)
 
     # Makes request to Google Distance Matrix API.
     def _distance_matrix_request(self, origin_list, destination_list):
         origin_str = self._create_places_string(origin_list)
         destination_str = self._create_places_string(destination_list)
-        return distance_matrix.distance_matrix(client=self._client, origins=origin_str, destinations=destination_str)
+        return distance_matrix.distance_matrix(
+            client=self._client, origins=origin_str, destinations=destination_str
+        )
 
     # Save a distance dict to json.
-    def _save_distance_dict(self, distance_dict):
-        file_name = "data/park_distances.json"
+    def _save_distance_dict(self, distance_dict, file_name):
         with open(file_name, "w") as fp:
             json.dump(distance_dict, fp)
-        print('Saved distances to json file.')
+        print("Saved distances to json file.")
 
-    def build_distance_matrix(self, read_existing_data=False, places=None):
-        '''
+    def _block_distance_matrix_request(
+        self, file_name, origin_ids, dest_ids, distance_dict=None
+    ):
+        if distance_dict is None:
+            distance_dict = {}
+        y_length = len(origin_ids)
+        x_length = len(dest_ids)
+        # Create requests in blocks of 10 x 10. Distance Matrix restricts max request size to 100 elements.
+        block_length = 10
+        y = 0
+        while y < y_length:
+            # As a precaution, save after each row.
+            if y > 0:
+                self._save_distance_dict(distance_dict, file_name=file_name)
+            end_y = min(y_length, y + block_length)
+            x = 0
+            while x < x_length:
+                print(f"Retrieving block with row {y}, column {x}.")
+                end_x = min(x_length, x + block_length)
+                rows = origin_ids[y:end_y]
+                cols = dest_ids[x:end_x]
+
+                try:
+                    # Make the request to Google Distance Matrix API.
+                    block_distance_matrix = self._distance_matrix_request(
+                        origin_list=rows, destination_list=cols
+                    )
+                except Exception as e:
+                    # In case the API breaks for whatever reason, we want to save what we have so far.
+                    self._save_distance_dict(distance_dict, file_name=file_name)
+                    print(f"Failed at: y = {y} and x = {x}.")
+                    print(f"Next time, we can rerun starting from this block.")
+                    raise Exception("Google Distance Matrix API failed.")
+
+                matrix_rows = block_distance_matrix["rows"]
+
+                for row_index, row in enumerate(matrix_rows):
+                    row_place_id = rows[row_index]
+                    elements = row["elements"]
+                    for col_index, dest in enumerate(elements):
+                        col_place_id = cols[col_index]
+                        val = "N/A"
+                        # Check if it is possible to go from origin to destination.
+                        if "distance" in dest:
+                            distance_in_metres = dest["distance"]["value"]
+                            if distance_in_metres == 0:
+                                # Same start and end point. Set to N/A.
+                                val = "N/A"
+                            else:
+                                distance_in_kilometers = distance_in_metres / 1000
+                                val = distance_in_kilometers
+                        if row_place_id in distance_dict:
+                            distance_dict[row_place_id][col_place_id] = val
+                        else:
+                            distance_dict[row_place_id] = {col_place_id: val}
+                x = end_x
+            y = end_y
+
+        self._save_distance_dict(distance_dict, file_name=file_name)
+        print("Finished retrieving Distance Matrix Data.")
+
+    def compute_cities_to_parks_distance(self):
+        """
         IMPORTANT!!!
         Google Distance Matrix API costs $4 usd 1000 elements; (origin, dest) pairs.
         It comes with $300 USD free credits to be spent in first 90 days.
         Places:
             200 places x 200 places = 40,000 elements = $160 USD. (new api key)
             70 cities x 200 places = 14,000 elements = $56 USD (old api key).
-        '''
+        DO NOT CALL THIS METHOD UNLESS YOU ARE 100% SURE ON HOW GCP BILLING WORKS!
+        """
+        input_val = input(
+            "Are you sure you want to call this method (expensive GCP cost)? Enter Y to continue."
+        )
+        if input_val not in {"Y", "y"}:
+            raise ValueError("Terminating as user did not confirm by entering 'Y'.")
+
+        with open("data/park_distances.json") as f:
+            park_distances = json.load(f)
+        with open("data/cities_to_place_id.json") as f2:
+            cities_to_place_id = json.load(f2)
+        cities = list(cities_to_place_id.values())
+        parks = list(park_distances.keys())
+        file_name = "data/city_place_ids_to_parks_distances.json"
+        self._block_distance_matrix_request(
+            file_name=file_name, origin_ids=cities, dest_ids=parks
+        )
+
+    def build_distance_matrix(self, read_existing_data=False, places=None):
+        """
+        IMPORTANT!!!
+        Google Distance Matrix API costs $4 usd 1000 elements; (origin, dest) pairs.
+        It comes with $300 USD free credits to be spent in first 90 days.
+        Places:
+            200 places x 200 places = 40,000 elements = $160 USD. (new api key)
+            70 cities x 200 places = 14,000 elements = $56 USD (old api key).
+        DO NOT CALL THIS METHOD UNLESS YOU ARE 100% SURE ON HOW GCP BILLING WORKS!
+        """
+        input_val = input(
+            "Are you sure you want to call this method (expensive GCP cost)? Enter Y to continue."
+        )
+        if input_val not in {"Y", "y"}:
+            raise ValueError("Terminating as user did not confirm by entering 'Y'.")
 
         with open("data/park_data.json") as f:
             park_data = json.load(f)
@@ -55,10 +150,7 @@ class GDistanceMatrix:
                     park_data.pop(place)
 
         parks = sorted(list(park_data.keys()))
-        place_ids = [park_data[park]['place_id'] for park in parks]
-        print(parks)
-        print(place_ids)
-        length = len(place_ids)
+        place_ids = [park_data[park]["place_id"] for park in parks]
 
         distance_dict = {}
         file_name = "data/park_distances.json"
@@ -67,61 +159,9 @@ class GDistanceMatrix:
             with open(file_name) as f:
                 distance_dict = json.load(f)
 
-        # Create requests in blocks of 10 x 10.
-        block_length = 10
-        y = 0
-        while y < length:
-            # As a precaution, save after each row.
-            if y > 0:
-                self._save_distance_dict(distance_dict)
-            end_y = min(length, y+block_length)
-            x = 0
-            while x < length:
-                print(f"Retrieving block with row {y}, column {x}.")
-                end_x = min(length, x+block_length)
-                rows = place_ids[y:end_y]
-                cols = place_ids[x:end_x]
-
-                try:
-                    # Make the request to Google Distance Matrix API.
-                    block_distance_matrix = self._distance_matrix_request(origin_list=rows, destination_list=cols)
-                except Exception as e:
-                    # In case the API breaks for whatever reason, we want to save what we have so far.
-                    self._save_distance_dict(distance_dict)
-                    print(f"Failed at: y = {y} and x = {x}.")
-                    print(f"Next time, we can rerun starting from this block.")
-                    raise Exception('Google Distance Matrix API failed.')
-
-                matrix_rows = block_distance_matrix['rows']
-
-                for row_index, row in enumerate(matrix_rows):
-                    row_place_id = rows[row_index]
-                    elements = row['elements']
-                    for col_index, dest in enumerate(elements):
-                        col_place_id = cols[col_index]
-                        val = 'N/A'
-                        # Check if it is possible to go from origin to destination.
-                        if 'distance' in dest:
-                            distance_in_metres = dest['distance']['value']
-                            if distance_in_metres == 0:
-                                # Same start and end point. Set to N/A.
-                                val = 'N/A'
-                            else:
-                                distance_in_kilometers = distance_in_metres / 1000
-                                val = distance_in_kilometers
-                        if row_place_id in distance_dict:
-                            distance_dict[row_place_id][col_place_id] = val
-                        else:
-                            distance_dict[row_place_id] = {col_place_id: val}
-                x = end_x
-            y = end_y
-
-        self._save_distance_dict(distance_dict)
-        print('Finished retrieving Distance Matrix Data.')
-
-
-
-
-
-
-
+        self._block_distance_matrix_request(
+            file_name=file_name,
+            origin_ids=place_ids,
+            dest_ids=place_ids,
+            distance_dict=distance_dict,
+        )
--- google_maps_services.py
+++ google_maps_services.py
@@ -1,6 +1,7 @@
-import urllib.request, json
+import json
 from keys import NPS_API_KEY
 import requests
+import pandas as pd
 from google_places import GPlaces
 from google_distance_matrix import GDistanceMatrix
 
@@ -16,10 +17,18 @@ class GMapsServices:
         self.g_distance_matrix = GDistanceMatrix(client=client)
 
     def _get_photo_urls(self, photos):
+        """
+        Helper to get the actual photo urls in a list of photo dictionaries.
+        :param photos: A list of dictionaries.
+        :return: A list of strings, where each string is the photo's url.
+        """
         return [photo["url"] for photo in photos]
 
-    # Get info for all national parks/monuments in USA. Makes request to National Park Services (NPS) API.
     def _get_usa_national_parks_request(self):
+        """
+        # Get info for all national parks/monuments in USA. Makes request to National Park Services (NPS) API.
+        :return: A dictionary containing the park names as keys, and its info as a value dictionary.
+        """
         # Configure API request
         # Note limit parameter is arbitrary value greater than total number of NPS sites
         endpoint = "https://developer.nps.gov/api/v1/parks?limit=600"
@@ -43,8 +52,12 @@ class GMapsServices:
             park_info[name] = info
         return park_info
 
-    # Download photos of the national park sites locally.
     def _download_photos(self, park_info):
+        """
+        # Download photos of the national park sites locally.
+        :param park_info: A dictionary containing park name, and other information (photos) as values in a dict.
+        :return: None
+        """
         keys = park_info.keys()
         num_parks = len(keys)
         park_index = 1
@@ -72,7 +85,35 @@ class GMapsServices:
             )
             park_index += 1
 
+    def _get_google_data_for_cities(self):
+        """
+        Reading in the cities from the csv file, map each city to a Google place_id.
+        :return: A dictionary of city name to place_id.
+        """
+        cities_to_place_id = {}
+        df = pd.read_csv("data/us_cities.csv")
+        df.reset_index(inplace=True)
+        cities = list(df["city"])
+        states = list(df["state_id"])
+        for index, city in enumerate(cities):
+            query = f"{city}, {states[index]}"
+            print(f"Searching Google for {query}")
+            gplaces_response = self.g_places.search_for_places(query=query)
+            place = gplaces_response["candidates"][0]
+            place_id = place["place_id"]
+            cities_to_place_id[query] = place_id
+        file_name = "data/cities_to_place_id.json"
+        with open(file_name, "w") as fp:
+            json.dump(cities_to_place_id, fp)
+        return cities_to_place_id
+
     def _get_google_data_for_parks(self, park_info):
+        """
+        Given a dictionary of park information, get the Google place_id, reviews and number of reviews
+        for each park.
+        :param park_info: A dict containing the park name as key, and other info inside a value dict.
+        :return: A new dictionary containing the additional Google fields.
+        """
         num_parks = len(park_info.keys())
         index = 1
         keys = list(park_info.keys())
@@ -99,8 +140,41 @@ class GMapsServices:
             index += 1
         return park_info
 
+    def get_nearest_city_to_place(
+        self, place_id, city_place_ids_to_parks_distances, place_ids_to_city
+    ):
+        """
+        For each park, get the nearest city and distance to nearest city. Store in park_place_id_to_nearest_city.json
+        :param place_id: The Google place_id of the park.
+        :param city_place_ids_to_parks_distances: Dict where key is a city place_id, val is a dict of park place_ids and distances.
+        :param place_ids_to_city: A dict mapping place_ids to city names.
+        :return: A tuple containing the nearest dist (in km) and nearest city name.
+        """
+        nearest_dist = "N/A"
+        nearest_city = "N/A"
+        for city_id, distances in city_place_ids_to_parks_distances.items():
+            try:
+                distance = distances[place_id]
+            except KeyError as ke:
+                print(f"No route from park {place_id} to {place_ids_to_city[city_id]}")
+                continue
+            if distance == "N/A":
+                continue
+            elif nearest_dist == "N/A":
+                nearest_dist = distance
+                nearest_city = place_ids_to_city[city_id]
+            else:
+                if distance < nearest_dist:
+                    nearest_dist = distance
+                    nearest_city = place_ids_to_city[city_id]
+        return nearest_dist, nearest_city
+
     def get_nps_raw_park_data(self, save_photos=False):
-        # Later, this should also get ratings and number of ratings for all the parks using Google Places API.
+        """
+        Make request to NPS API to get all park data. Saves data in data/nps_raw_park_data.json file.
+        :param save_photos: Whether or not to save the photos to local disk.
+        :return: A dictionary containing all the park information available from NPS.
+        """
         park_info = self._get_usa_national_parks_request()
         # Then, use requests to download all the photos.
         if save_photos:
@@ -111,6 +185,12 @@ class GMapsServices:
         return park_info
 
     def get_all_national_parks(self, save_photos=False):
+        """
+        Get all park data from the NPS API, and get the Google place_ids, reviews, and number of reviews
+        for each place. Save all this information in data/park_data.json.
+        :param save_photos: whether or not to download the park photos from NPS to local disk.
+        :return: None
+        """
         park_info = self.get_nps_raw_park_data(save_photos=save_photos)
 
         # Now, use Google Places to get the place_id, rating, and user_ratings_total for each park.
@@ -119,8 +199,11 @@ class GMapsServices:
         with open(file_name, "w") as fp:
             json.dump(detailed_park_info, fp)
 
-    # This method tries to find the parks that were not able to be found by Google.
     def fill_missing_park_data(self):
+        """
+        # This method tries to find the parks that were not able to be found by Google.
+        :return: None
+        """
         with open("data/nps_raw_park_data.json") as f:
             raw_park_data = json.load(f)
         with open("data/park_data.json") as f2:
@@ -171,19 +254,97 @@ class GMapsServices:
             json.dump(park_data_with_google, fp)
 
     def rank_places_by_reviews(self, limit=200):
+        """
+        Given a limit, retrieve the top {limit} places in the park_data.json file by number of reviews.
+        :param limit: The top number of rows.
+        :return: A list of the names of the top {limit} places.
+        """
         with open("data/park_data.json") as f:
             park_data_with_google = json.load(f)
-            park_data_with_google.pop('Ellis Island Part of Statue of Liberty National Monument') # duplicate
+            park_data_with_google.pop(
+                "Ellis Island Part of Statue of Liberty National Monument"
+            )  # duplicate
             sorted_by_num_ratings = sorted(
                 park_data_with_google.items(),
                 key=lambda x: x[1]["num_ratings"],
                 reverse=True,
             )
             for index, data in enumerate(sorted_by_num_ratings):
-                print(f"Rank: {index}, Ratings: {data[1]['num_ratings']}, Name: {data[0]}")
+                print(
+                    f"Rank: {index}, Ratings: {data[1]['num_ratings']}, Name: {data[0]}"
+                )
             top_results = sorted_by_num_ratings[0:limit]
             return [data[0] for data in top_results]
 
+    def map_place_id_to_cities(self):
+        """
+        Create a json mapping place_id to city based on the cities_to_place_id dictionary.
+        :return: None
+        """
+        with open("data/cities_to_place_id.json") as f:
+            city_to_place_id = json.load(f)
+        place_id_to_city = {v: k for k, v in city_to_place_id.items()}
+        file_name = "data/place_ids_to_city.json"
+        with open(file_name, "w") as fp:
+            json.dump(place_id_to_city, fp)
+
+    def map_place_id_to_park_name(self):
+        """
+        Create a json mapping place_id to park based on the park_data dictionary.
+        :return: None
+        """
+        with open("data/park_data.json") as f:
+            park_to_info = json.load(f)
+        place_id_to_park_name = {v["place_id"]: k for k, v in park_to_info.items()}
+        file_name = "data/place_ids_to_park_name.json"
+        with open(file_name, "w") as fp:
+            json.dump(place_id_to_park_name, fp)
+
+    def compute_nearest_city_for_each_park_place_id(self):
+        """
+        Create a dictionary that has the nearest city and distance to city for each park place_id.
+        :return: None
+        """
+        with open("data/place_ids_to_city.json") as f:
+            place_ids_to_city = json.load(f)
+        with open("data/city_place_ids_to_parks_distances.json") as f2:
+            city_to_park_distances = json.load(f2)
+        with open("data/place_ids_to_park_name.json") as f3:
+            place_ids_to_parks = json.load(f3)
+        with open("data/park_distances.json") as f4:
+            park_distances = json.load(f4)
+        park_ids = list(park_distances.keys())
+
+        park_id_to_nearest_city = {}
+        for park_id in park_ids:
+            nearest_dist, nearest_city = self.get_nearest_city_to_place(
+                place_id=park_id,
+                city_place_ids_to_parks_distances=city_to_park_distances,
+                place_ids_to_city=place_ids_to_city,
+            )
+            park_id_to_nearest_city[park_id] = {
+                "park_name": place_ids_to_parks[park_id],
+                "distance_to_city": nearest_dist,
+                "nearest_city": nearest_city,
+            }
+        file_name = "data/park_id_to_nearest_city.json"
+        with open(file_name, "w") as fp:
+            json.dump(park_id_to_nearest_city, fp)
+
+    def compute_distances_for_cities(self, get_city_place_ids=False):
+        """
+        Computes the distance from each city to each park.
+        :param get_city_place_ids: whether to call Google Places API to get the place_ids for each city.
+        :return:
+        """
+        if get_city_place_ids:
+            self._get_google_data_for_cities()
+        self.g_distance_matrix.compute_cities_to_parks_distance()
+
     def compute_distances(self):
+        """
+        Make request to the Google Distance Matrix API to compute distances between the places.
+        :return: None
+        """
         top_places = self.rank_places_by_reviews()
         self.g_distance_matrix.build_distance_matrix(places=top_places)
